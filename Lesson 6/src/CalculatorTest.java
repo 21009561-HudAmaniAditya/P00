1100035ad3584eafe1e57f4d62ec54e1261b12f1
21009561 <21009561@172.29.72.115>
2022-06-03 14:11:15 +0800

    commit

---
+++ Lesson 6/src/CalculatorTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+	public void testAdd() {
+		int a = 4321;
+		int b = 1234;
+			
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b); 
+			 
+		int expected = 5555;
+		assertEquals (expected, actual);
+		}
+}
+
+
